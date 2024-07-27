8dfff6c9796ff4725cc39d0c948fd9825f7c5ae0
Aayush-Kr-13 <2301aayush@gmail.com>
2024-07-28 01:29:47 +0530

    vercel analytics added

--- Destination-Planner/src/main.jsx
+++ Destination-Planner/src/main.jsx
@@ -9,6 +9,7 @@ import { Toaster } from './components/ui/toaster.jsx'
 import { ClerkProvider } from '@clerk/clerk-react'
 import Viewtrip from './view-trip/[tripId]/index.jsx'
 import MyTrip from './my-trips/index.jsx'
+import { Analytics } from "@vercel/analytics/react"
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 
@@ -38,6 +39,7 @@ const router = createBrowserRouter([
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
+      <Analytics/>
       <Header/>
       <Toaster/>
       <RouterProvider router={router}/>
